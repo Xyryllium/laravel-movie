6586a46cd5533dae1e4b569682fd2e4f1f14c227
Xy <xyaranzaaa@gmail.com>
2020-10-15 13:19:26 +0800

    Added Alpine.Js for UI interactivity

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,5 +1,21 @@
-<div class="relative mt-3 md:mt-0">
-    <input wire:model.debounce.500ms="search" type="text" class="bg-blue-800 text-sm rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline" placeholder="Search">
+<div class="relative mt-3 md:mt-0" x-data="{ isOpen: true }" @click.away="isOpen  = false">
+    <input
+     wire:model.debounce.500ms="search"
+     type="text"
+     class="bg-blue-800 text-sm rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline"
+     placeholder="Search"
+     x-ref="search"
+     @keydown.window="
+        if(event.keyCode === 191){
+            event.preventDefault();
+            $refs.search.focus();
+        }
+     "
+     @focus="isOpen = true"
+     @keydown="isOpen = true"
+     @keydown.escape.window="isOpen = false"
+     @keydown.shift.tab="isOpen = false"
+     >
     <div class="absolute top-0">
         <svg class="fill-current w-4 ml-2 text-gray-500" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
         width="28.931px" height="28.932px" viewBox="0 0 28.931 28.932" style="enable-background:new 0 0 28.931 28.932;"
@@ -16,12 +32,19 @@
     <div wire:loading class="spinner top-0 right-0 mr-4 mt-3"></div>
 
     @if (strlen($search) > 2)
-        <div class="absolute bg-blue-800 text-sm rounded w-64 mt-4">
+        <div
+         class="z-50 absolute bg-blue-800 text-sm rounded w-64 mt-4"
+         x-show.transition.opacity="isOpen"
+         >
             @if ($searchResults->count() > 0)
                 <ul>
                     @foreach ($searchResults as $result)
                         <li class="border-b border-blue-700">
-                            <a href="{{route('movies.show', $result['id'])}}" class="hover:bg-blue-700 px-3 py-3 flex items-center">
+                            <a
+                            href="{{route('movies.show', $result['id'])}}"
+                            class="hover:bg-blue-700 px-3 py-3 flex items-center transition ease-in-out duration-150"
+                            @if ($loop->last) @keydown.tab="isOpen = false" @endif
+                            >
                                 @if ($result['poster_path'])
                                     <img src="https://image.tmdb.org/t/p/w92/{{$result['poster_path']}}" class="w-8" alt="{{$result['title']}}">
                                 @else
